abbe1ee7516f5f04b40221b0d9fef9562211f3dc
niceyeti <jesse_waite@hotmail.com>
2022-07-19 18:27:52 -0700

    Updates

--- main.go
+++ main.go
@@ -113,27 +113,32 @@ big_course = ['WWWWWWWWWWWWWWWWWW',
 */
 
 // Converts a tack input string array to an actual state grid of positions and velocities.
+// The orientation is such that the bottom/left most position of the track (when printed in a console) is (0,0).
+// This gives awkward reverse-iteration displaying, but makes sense for the problem dynamics: +1 velocity yields +1 position in some array.
 // Note that this is just an (X x Y x VX x VY) size matrix and would be implemented as such in Python.
 // Note there is no error checking on the input track, nor error returned.
 // Returns: multidim state slice, whose indices are [x][y][vx][vy].
 func convert_track(track []string) (states [][][][]State) {
-	height := len(track)
 	width := len(track[0])
+	height := len(track)
 
-	states = [][][][]State{}
-	for x := 0; x < height; x++ {
-		states = append(states, [][][]State{})
-		for y := 0; y < width; y++ {
-			states[x] = append(states[x], [][]State{})
+	states = make([][][][]State, 0, width)
+	// Build cells from left to right...
+	for x := 0; x < width; x++ {
+		states = append(states, make([][][]State, 0, height))
+		// And bottom to top...
+		for y := 0; y < height; y++ {
+			states[x] = append(states[x], make([][]State, 0, 5))
 			// Augment the track cell with x/y velocity values per each state
 			for vx := 0; vx < 5; vx++ {
-				states[x][y] = append(states[x][y], []State{})
+				states[x][y] = append(states[x][y], make([]State, 0, 5))
 				for vy := 0; vy < 5; vy++ {
 					state := State{
 						x : x,
 						y : y,
 						vx : vx,
 						vy : vy,
+						cell_type: rune(track[height - y - 1][x]),
 						value : 0,
 					}
 					states[x][y][vx] = append(states[x][y][vx], state)
@@ -152,25 +157,50 @@ func convert_track(track []string) (states [][][][]State) {
 // html, but for displaying in a console this is truncated by simply displaying direction based on
 // the maximum vx/vy value as ^, >, v, <.
 func show_policy(states [][][][]State) {
-	for _, row := range states {
+	for y := range rev(len(states[0])) {
 		fmt.Print(" ")
-		for _, vel_states := range row {
-			dir := max_dir(vel_states)
+		for x := range states {
+			dir := max_dir(states[x][y])
 			fmt.Printf("%c ", dir)
 		}
 		fmt.Println("")
 	}
 }
 
+// Returns reversed indices of a slice, e.g. for ranging over.
+func rev(length int) []int {
+	indices := make([]int, length)
+	for i := 0; i < length; i++ {
+		indices[i] = length - i - 1
+	}
+	return indices
+}
+
 // Prints the maximum vx or vy value for each x/y position in the state set.
 // Note that this truncates some info, since only one of these orthogonal values sets is displayed;
 // this just allows showing progress.
 func show_max_values(states [][][][]State) {
-	for _, row := range states {
+	for y := range rev(len(states[0])) {
 		fmt.Print(" ")
-		for _, vel_states := range row {
-			state := max_vel_state(vel_states)
+		for x := range states {
+			velstates := states[x][y]
+			state := max_vel_state(velstates)
 			fmt.Printf("%.1f ", state.value)
+		}
+		fmt.Println("")
+	}
+}
+
+// Purely for debugging: print the entire state structs.
+func show_all(states [][][][]State, fn func(s *State) string) {
+	for y := range rev(len(states)) {
+		for x := range states {
+			for _, vxs := range states[x][y] {
+				for _, state := range vxs {
+					fmt.Printf("%s ", fn(&state))
+				}
+			}
+			fmt.Println("")
 		}
 		fmt.Println("")
 	}
@@ -192,11 +222,12 @@ func max_dir(vel_states[][]State) rune {
 func max_vel_state(vel_states[][]State) (max_state *State) {
 	// Get the max value from the state subset of velocities
 	max_state = &State {
-		value: math.SmallestNonzeroFloat64,
+		value: -math.MaxFloat64,
 	}
 
-	for _, vxstates := range vel_states {
-		for _, state := range vxstates {
+	for vx := range vel_states {
+		for _, state := range vel_states[vx] {
+			//fmt.Printf("%.1f - \n", state.value)
 			if state.value >= max_state.value {
 				max_state = &state
 			}
@@ -215,9 +246,9 @@ func init_state_values(states [][][][]State, value float64) {
 func mutate(states [][][][]State, fn func(s *State)) {
 	for _, row := range states {
 		for _, col := range row {
-			for _, vx := range col {
-				for _, state := range vx {
-					fn(&state)
+			for _, vxs := range col {
+				for i := range vxs {
+					fn(&vxs[i])
 				}
 			}
 		}
@@ -226,7 +257,7 @@ func mutate(states [][][][]State, fn func(s *State)) {
 
 func main() {
 	// choose/input a track
-	racetrack := track 
+	racetrack := track
 	// convert to state space
 	states := convert_track(racetrack)
 	// initialize the state values to something slightly larger than the lowest reward, for stability
@@ -235,4 +266,6 @@ func main() {
 	show_policy(states)
 	// show max values
 	show_max_values(states)
+
+	//show_all(states, func(s *State) string { return fmt.Sprintf("%.1f\n", s.value)})
 }
